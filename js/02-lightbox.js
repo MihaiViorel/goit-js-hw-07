c09ecdedd76e99bda1fc5e3e615e0c9c23e86550
MihaiViorel <cristeimihaiviorel@gmail.com>
2024-01-14 15:05:45 +0200

    all tasks done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,67 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Cerinta Exercitiul 2:
 
-console.log(galleryItems);
+// Creați aceeași galerie ca în prima sarcină, folosind librăria SimpleLightbox, care se va ocupa de procesarea click - urilor pe imagini, de deschiderea și închiderea unei ferestre modale și de listarea imaginilor, folosind tastatura.Puteți urmări filmulețul demonstrativ al galeriei cu biblioteca conectată.
+
+// Modificați puțin aspectul cardului de galerie, folosind acest șablon.
+// <li class="gallery__item">
+//    <a class="gallery__link" href="large-image.jpg">
+//       <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+//    </a>
+// </li >
+  
+// Îndepliniți acest task în fișierele 02 - lightbox.html și 02 - lightbox.js.Împărțiți - l în mai multe subtask - uri:
+
+// - Crearea și randarea unui marcaj pe baza datelor din matricea de date galleryItems și a șablonului de articol furnizat din galerie. Refolosiți codul scris din primul exercițiu.
+// - Conectarea scriptului și a stilurilor librăriei, CDN service cdnjs. Adăugați link-urile pentru fișierele: simple-lightbox.min.js și simple-lightbox.min.css.
+// - Inițializarea librăriei după ce elementele galeriei sunt create și adăugate în ul.gallery. Pentru a face acest lucru, citiți documentația SimpleLightbox, secțiunile "Usage" și "Markup".
+// - Căutați în documentație secțiunea "Options" și adăugați un text sugestiv imaginei în atributul alt. Textul alternativ va fi poziționat în partea de jos și va apărea la 250 de milisecunde după deschiderea imaginii.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+import { galleryItems } from './gallery-items.js';
+
+const galleryList = document.querySelector('.gallery');
+
+function galleryElementCreation(item) {
+  const galleryItem = document.createElement('li');
+  const galleryLink = document.createElement('a');
+  const galleryImage = document.createElement('img');
+
+  galleryItem.classList.add('gallery__item');
+  galleryLink.classList.add('gallery__link');
+  galleryImage.classList.add('gallery__image');
+
+  galleryLink.href = item.original;
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+  galleryImage.dataset.source = item.original;
+
+  galleryItem.appendChild(galleryLink);
+  galleryLink.appendChild(galleryImage);
+
+  return galleryItem;
+}
+
+galleryItems.forEach(item => {
+  const galleryItem = galleryElementCreation(item);
+  galleryList.appendChild(galleryItem);
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", (clickEvent) => {
+  clickEvent.preventDefault();
+  lightbox.open();
+});
+
+document.addEventListener('keydown', (escEvent) => {
+  if (escEvent.key === 'Escape' && lightbox.isOpen) {
+    lightbox.close();
+  }
+});
--- js/task1-comments.js
+++ js/task1-comments.js
@@ -19,34 +19,54 @@
 
 import { galleryItems } from './gallery-items.js';
 
+// Selectarea tag-ului ul
 const galleryList = document.querySelector('.gallery');
 
+// Crearea unei functii ce are ca si scop alocarea tag-urilor/ claselor/ content-ului pentru elementul listei
 function galleryElementCreation(item) {
+  // Creare tag-uri HTML (li,a,img)
   const galleryItem = document.createElement('li');
   const galleryLink = document.createElement('a');
   const galleryImage = document.createElement('img');
+  // Creare clase CSS (.gallery__item,.gallery__image,.gallery__link)
   galleryItem.classList.add('gallery__item');
   galleryLink.classList.add('gallery__link');
   galleryImage.classList.add('gallery__image');
-  galleryLink.href = item.original;
-  galleryImage.src = item.preview;
-  galleryImage.alt = item.description;
-  galleryImage.dataset.source = item.original; 
+  // Crearea Atributelor tag-urilor
+  galleryLink.href = item.original; // link-ul catre dimensiunea originala a imaginii (regasita in gallery-items.js)
+  galleryImage.src = item.preview; // imaginea de thumbnail (regasita in gallery-items.js)
+  galleryImage.alt = item.description; // textul afisat daca nu se incarca iamginea (regasit in gallery-items.js)
+  galleryImage.dataset.source = item.original; // Salvare url-ului imaginii originale in atributul data-src al tag-ului img, link-ul ramane invizibil pt browser pana va fi accesat in fereastra modala (este o modalitate de stocare a link-ului)
+  // Crearea ierarhiei parinte-copil-descendent (li->a->img sau galleryItem->galleryLink->galleryImage)
   galleryItem.appendChild(galleryLink);
   galleryLink.appendChild(galleryImage);
+  // Returnarea elementului final li
   return galleryItem;
 }
 
+// Crearea unei bucle care sa itereze prin fiecare element al array-ului 'galleryItems' din fisierul gallery-items.js
 galleryItems.forEach(item => {
+  // ^ Alegem array-ul 'galleryItems' din fisierul gallery-items.js, folosim 'forEach' ca sa iteram prin fiecare element al array-ului, dam un nume pt fiecare element al array-ului: 'item'.
   const galleryItem = galleryElementCreation(item);
+  // ^ Cream o constanta cu rolul de a stoca elementul li obtinut prin apelarea functiei creata anterior
   galleryList.appendChild(galleryItem);
+  // ^ Realizam ierarhia Parinte-Copil intre elementul ul si li (sau galleryList si galleryItem)
 });
 
+// Delegarea evenimentului on click si crearea ferestrei modale
 galleryList.addEventListener('click', (clickEvent) => {
-  clickEvent.preventDefault();
+  // ^ Delegarea evenimentului se face la nivelul ul pt a evita declararea evenimentului pt fiecare li
+  clickEvent.preventDefault(); // anulam actiunea ce sar fi intamplat by default la accesarea link-ului
 
   if (clickEvent.target.nodeName === 'IMG') {
-    const imageUrl = clickEvent.target.dataset.source;
+    // ^ clickEvent.target : obiectul din DOM care a declansat evenimentul de click
+    // ^ clickEvent.target.nodeName : pentru a obtine numele tag-ului HTML ce a declansat evenimentul de click ("IMG")
+    const imageUrl = clickEvent.target.dataset.source; // colectam imaginea originala din atributul data-src al tag-ului img
+    
+    // In continuare folosim bibloteca "basicLightbox" pentru a crea fereastra modala
+    // basicLightbox.create - o metoda a librariei "basicLightbox" ce primeste ca argument un sir de caracte care contine marcajul HTML al continutului ferestrei modale
+    // realizam inchiderea feerestrei modale la apasarea tastei Escape
+    // metoda show() afiseaza fereastra modala 'instace' creata cu ajutorul librariei basicLightbox
     const instance = basicLightbox.create(`
       <img src="${imageUrl}" width="800" height="600">
     `);
